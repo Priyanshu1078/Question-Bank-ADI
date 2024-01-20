09cf2095f5a524c57694877b3fe49d6daa3e5c7b
Priyanshu1078 <101087607+Priyanshu1078@users.noreply.github.com>
2024-01-20 22:34:28 +0530

    Create ques34(egg dropping puzzle).java

---
+++ ques34(egg dropping puzzle).java
@@ -0,0 +1,27 @@
+//(DP+Binary Search)
+import java.util.Scanner;
+public class ques34 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        Integer[][] dp=new Integer[k+1][n+1];
+        System.out.println(sol(k,n,dp));
+    }
+    public static int sol(int k,int n,Integer[][] dp){
+        if(k==1 || n==0 || n==1) return n;
+        if(dp[k][n]!=null) return dp[k][n];
+        int l=1,h=n;
+        int ans=Integer.MAX_VALUE;
+        while(l<=h){
+            int mid=(l+h)/2;
+            int k1=sol(k-1,mid-1,dp);
+            int k2=sol(k,n-mid,dp);
+            int temp=1+Math.max(k1,k2);
+            if(k1<k2) l=mid+1;
+            else h=mid-1;
+            ans=Math.min(temp,ans);
+        }
+        return dp[k][n]=ans;
+    }
+}
