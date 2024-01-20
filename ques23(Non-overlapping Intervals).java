6f3b359c3dc7de6486f8dad9d556911b956946e8
Priyanshu1078 <101087607+Priyanshu1078@users.noreply.github.com>
2024-01-20 15:25:43 +0530

    Create ques23(Non-overlapping Intervals).java

---
+++ ques23(Non-overlapping Intervals).java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class ques23 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[][] arr=new int[n][2];
+        for(int i=0;i<n;i++){
+            arr[i][0]=sc.nextInt();
+            arr[i][1]=sc.nextInt();
+        }
+        Arrays.sort(arr,(a,b) -> a[0]-b[0]);
+        int ans=0,end=arr[0][1];
+        for(int i=1;i<n;i++){
+            if(arr[i][0]<end){
+                ans++;
+                end=Math.min(arr[i][1],end);
+            }else end=arr[i][1];
+        }
+        System.out.println(ans);
+    }
+}
