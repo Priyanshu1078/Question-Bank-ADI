b2937357552a6e1c478855c5029ba3c793437d1a
Priyanshu1078 <101087607+Priyanshu1078@users.noreply.github.com>
2024-01-20 18:16:35 +0530

    Update ques6(LCA).java

--- ques6(LCA).java
+++ ques6(LCA).java
@@ -4,21 +4,20 @@ public class ques6 {
         int val;
         TreeNode left;
         TreeNode right;
-        TreeNode(){}
         TreeNode(int val){
             this.val=val;
         }
     }
-    static int idx=0;
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        String s=sc.nextLine(); //array size is not given so we are taking input array as String and converting it in Integer Array or you can use sc.hasNext().
+        String s=sc.nextLine();
         int p=sc.nextInt();
         int q=sc.nextInt();
         String str[]=s.split(" ");
         int arr[]=new int[str.length];
         for(int i=0;i<arr.length;i++) arr[i]=Integer.parseInt(str[i]);
-        TreeNode root=sol(arr,Integer.MAX_VALUE);
+        TreeNode root=sol(null,arr[0]);
+        for(int i=1;i<arr.length;i++) root=sol(root,arr[i]);
         System.out.println(sol(root,p,q));
     }
     public static int sol(TreeNode root,int p,int q){
@@ -30,14 +29,10 @@ public class ques6 {
         else if(left!=-1) return left;
         return right;
     }
-    public static TreeNode sol(int[] pre,int max){
-        if(idx>=pre.length || pre[idx]>max){
-            return null;
-        }
-        TreeNode root=new TreeNode(pre[idx]);
-        idx++;
-        root.left=sol(pre,root.val);
-        root.right=sol(pre,max);
+    public static TreeNode sol(TreeNode root, int k){
+        if(root==null) return new TreeNode(k);
+        if(root.val>k) root.left=sol(root.left,k);
+        else if(root.val<k) root.right=sol(root.right,k);
         return root;
     }
 }
