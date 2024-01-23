8f9a3de7373ba8cb83a3c7b25993213281211c80
Priyanshu <101087607+Priyanshu1078@users.noreply.github.com>
2024-01-24 02:37:38 +0530

    Update ques14(special elements).java

--- ques14(special elements).java
+++ ques14(special elements).java
@@ -11,35 +11,19 @@ public class ques14 {
         }
         int arr[]=new int[n];
         for(int i=0;i<n;i++) arr[i]=Integer.parseInt(str[i]);
-        int[][] lsum=new int[n][n];
-        int[][] rsum=new int[n][n];
-        lsum[0][0]=arr[0];
-        for(int i=1;i<n;i++){
+        int n=arr.length,ans=0,even=0,odd=0;
+        for(int i=0;i<n;i++){
+            if(i%2==0) even+=arr[i];
+            else odd+=arr[i];
+        }
+        int prev=0;
+        for(int i=0;i<n;i++){
             if(i%2==0){
-                lsum[i][0]=lsum[i-1][0]+arr[i];
-                lsum[i][1]=lsum[i-1][1];
-            }else{
-                lsum[i][0]=lsum[i-1][0];
-                lsum[i][1]=lsum[i-1][1]+arr[i];
-            }
+                even=even-arr[i]+prev;
+            }else odd=odd-arr[i]+prev;
+            if(even==odd) ans++;
+            prev=arr[i];
         }
-        if((n-1)%2==0) rsum[n-1][0]=arr[n-1];
-        else rsum[n-1][1]=arr[n-1];
-        for(int i=n-2;i>=0;i--){
-            if(i%2==0){
-                rsum[i][0]=rsum[i+1][0]+arr[i];
-                rsum[i][1]=rsum[i+1][1];
-            }else{
-                rsum[i][0]=rsum[i+1][0];
-                rsum[i][1]=rsum[i+1][1]+arr[i];
-            }
-        }
-        int c=0;
-        if(rsum[1][0]==rsum[1][1]) c++;
-        if(lsum[n-2][0]==lsum[n-2][1]) c++;
-        for(int i=1;i<n-1;i++){
-            if(lsum[i-1][0]+rsum[i+1][1]==lsum[i-1][1]+rsum[i+1][0]) c++;
-        }
-        System.out.println(c);
+        System.out.println(ans);
     }
 }
