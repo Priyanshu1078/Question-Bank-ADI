66d20b38e24efa7504e6755a2bae8b41c0c579e1
Priyanshu <101087607+Priyanshu1078@users.noreply.github.com>
2024-01-27 19:15:43 +0530

    Create ques7(Crack the Passcode).java

---
+++ ques7(Crack the Passcode).java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class ques7 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        String ans="";
+        for(int i=0;i<s.length()/3;i++){
+            if(isValid(s,s.substring(0,i+1))){
+                ans=s.substring(0,i+1);
+            }
+        }
+        System.out.println(ans);
+    }
+    public static boolean isValid(String s,String t){
+        int idx=s.indexOf(t);
+        if(idx==-1 || idx>0) return false;
+        int l=t.length();
+        s=s.substring(l);
+        idx=s.indexOf(t);
+        if(idx==-1 || idx>0) return false;
+        idx=s.lastIndexOf(t);
+        return idx==s.length()-t.length();
+    }
+}
