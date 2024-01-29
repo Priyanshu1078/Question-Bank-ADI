b72c66a6ea9974da6b9babecdec7156603a5d877
Priyanshu <101087607+Priyanshu1078@users.noreply.github.com>
2024-01-29 19:32:53 +0530

    Update ques34(egg dropping puzzle).java

--- ques34(egg dropping puzzle).java
+++ ques34(egg dropping puzzle).java
@@ -25,3 +25,14 @@ public class ques34 {
         return dp[k][n]=ans;
     }
 }
+//bottom-up
+public int sol(int k, int n) {
+    int[][] dp=new int[k+1][n+1];
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=k;j++){
+            dp[j][i]=dp[j-1][i-1]+dp[j][i-1]+1;
+            if(dp[j][i]>=n) return i;
+        }
+    }
+    return -1;
+}
