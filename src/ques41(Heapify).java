56e9d2e4022b6ddc0308f0e0a6dae683e48408e4
Priyanshu <101087607+Priyanshu1078@users.noreply.github.com>
2024-07-03 21:52:58 +0530

    Update ques41(Heapify).java

--- src/ques41(Heapify).java
+++ src/ques41(Heapify).java
@@ -1,21 +1,13 @@
 import java.util.*;
-import java.util.PriorityQueue;
 public class ques41 {
-    public static class myQueue extends PriorityQueue {
-        public void print() {
-            for (Object i : this) {
-                System.out.print(i+" ");
-            }
-        }
-    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        myQueue pq=new myQueue();
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
         for(int i=0;i<n;i++){
             int x=sc.nextInt();
             pq.add(x);
         }
-        pq.print();
+        for(int i:pq) System.out.print(i+" ");
     }
 }
