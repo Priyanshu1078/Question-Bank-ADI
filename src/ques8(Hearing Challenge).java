10a1baf92cc822106a64d8907dd0a11498b75521
Priyanshu <101087607+Priyanshu1078@users.noreply.github.com>
2024-07-04 21:02:20 +0530

    Update ques8(Hearing Challenge).java

--- src/ques8(Hearing Challenge).java
+++ src/ques8(Hearing Challenge).java
@@ -13,20 +13,13 @@ public class ques8 {
         }
     }
     public static boolean isValid(String s){
-        String x="Yes";
-        if(x.indexOf(s)!=-1) return true;
-        for(int i=0;i<s.length();i++){
-            if(x.indexOf(s.charAt(i))==-1){
-                return false;
-            }
+        for(int i=1;i<s.length();i++){
+            char ch=s.charAt(i),prev=s.charAt(i-1);
+            if(ch=='Y' && prev=='s') continue;
+            else if(ch=='e' && prev=='Y') continue;
+            else if(ch=='s' && prev=='e') continue;
+            return false;
         }
-        int c=1;
-        String a="Yes";
-        while(c<=s.length()/a.length()+2){
-            if(x.indexOf(s)!=-1) return true;
-            x=x+a;
-            c++;
-        }
-        return false;
+        return true;
     }
 }
